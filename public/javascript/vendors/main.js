463bfd7a7c35d72176e5ac8554e2abc5e6ba9cf5
TheKipmaster <fkipman@hotmail.com>
2021-06-17 17:21:36 -0300

    Implements bottom section and refactors styles

--- public/javascript/vendors/main.js
+++ public/javascript/vendors/main.js
@@ -1,9 +1,21 @@
-var flkty = new Flickity('.gallery', {
-  autoPlay: true,
-  wrapAround: true,
-  imagesLoaded: true,
-  lazyLoad: 2
-});
+var galleryElems = document.querySelectorAll('.gallery');
+
+for ( var i=0, len = galleryElems.length; i < len; i++ ) {
+  var galleryElem = galleryElems[i];
+  new Flickity( galleryElem, {
+    autoPlay: true,
+    wrapAround: true,
+    imagesLoaded: true,
+    lazyLoad: 2
+  });
+}
+
+// var flkty = new Flickity('.gallery', {
+//   autoPlay: true,
+//   wrapAround: true,
+//   imagesLoaded: true,
+//   lazyLoad: 2
+// });
 
 window.addEventListener('load', (event) => {
   flkty.resize();
